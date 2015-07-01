7a1f8279fa9d2a315cdc53c2c9cd8415b7246c3c
Zach Ramsay <zach@coinculture.info>
2015-07-01 19:47:41 -0400

    verification in browser

--- ajax.js
+++ ajax.js
@@ -19,6 +19,7 @@ function request_callback(xObj, _func, args){
 function make_request(xmlhttp, method, path, async, params){
 	xmlhttp.open(method, path, async);
 	xmlhttp.setRequestHeader("Content-Type", "application/json");
+console.log("paramss", params);
 	xmlhttp.send(JSON.stringify(params));
 }
 
@@ -27,13 +28,61 @@ function sendinfo() {
 	var auth = document.getElementById("password").value;
 	console.log("name & pass ", name, auth);
 	
+	nonce = makeNonce();
+	console.log("nonce", nonce);
+
 	xmlhttp = new_request_obj();
 	request_callback(xmlhttp, callback, []);
-	make_request(xmlhttp, "POST", "http://localhost:4767/gen", true, {"auth":auth, "name":name});
+	make_request(xmlhttp, "POST", "http://localhost:4767/name", true, {"name":name});
 
 }
 
 function callback() {
-	console.log("callback");
+	var response = JSON.parse(xmlhttp.responseText);
+	console.log("repsonse is here", response);
+	
+	addr = response.Response;
+	var err = response.Error;
+
+	var hash = nonce;
+	console.log("hahs", hash);
+
+	console.log("addr & err", addr, err);
+
+	xmlhttp = new_request_obj();
+	request_callback(xmlhttp, callback2, []);
+	make_request(xmlhttp, "POST", "http://localhost:4767/sign", true, {"addr":addr, "hash":hash });
 }
 
+function callback2(){
+	console.log("callback2");
+	var response = JSON.parse(xmlhttp.responseText);
+	console.log("signature", response);
+
+	var hash = nonce;
+	
+	var sig = response.Response;
+	var err = response.Error;
+
+	xmlhttp = new_request_obj();
+	request_callback(xmlhttp, callback3, []);
+	make_request(xmlhttp, "POST", "http://localhost:4767/verify", true, {"addr":addr, "hash":hash, "sig":sig });
+
+}
+
+function callback3() {
+	console.log("callback3");
+	var response = JSON.parse(xmlhttp.responseText);
+	console.log("verify", response);
+
+}
+
+function makeNonce() {
+	var text = "";
+	//hex only for now
+	var possible = "ABCDEFabcdef0123456789";
+	
+	for( var i=0; i < 10 ; i++ )
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	return text;
+}
